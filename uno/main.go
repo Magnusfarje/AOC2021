a71bfd2016751df313edae66a61ee7ab7f2cd642
Magnus Färje <magnus.farje@gmail.com>
2021-12-02 14:50:26 +0100

    Refactored day1 solution

--- uno/main.go
+++ uno/main.go
@@ -2,40 +2,24 @@ package main
 
 import "fmt"
 
-func NumOfIncreasesInSlice(numbers []int) int {
+func CountIncreasesBetweenIndexes(numbers []int, indexStep int) int {
 	increases := 0
 	for i, _ := range numbers {
-		if i == 0 {
-			continue
+		if len(numbers) <= i+indexStep {
+			break
 		}
-		if numbers[i] > numbers[i-1] {
+		if numbers[i] < numbers[i+indexStep] {
 			increases += 1
 		}
 	}
 	return increases
 }
 
-func SumSliceByRange(numbers []int, _range int) []int {
-	sumSlice := []int{}
-	for i, _ := range numbers {
-		if len(numbers) <= i+_range {
-			break
-		}
-
-		sum := 0
-		for j := 0; j <= _range; j++ {
-			sum += numbers[i+j]
-		}
-		sumSlice = append(sumSlice, sum)
-	}
-	return sumSlice
-}
-
 func main() {
 	assignmentData := []int{}
 
 	// Assignment 1
-	fmt.Println(NumOfIncreasesInSlice(assignmentData))
+	fmt.Println(CountIncreasesBetweenIndexes(assignmentData, 1))
 	// Assignment 2
-	fmt.Println(NumOfIncreasesInSlice(SumSliceByRange(assignmentData, 2)))
+	fmt.Println(CountIncreasesBetweenIndexes(assignmentData, 3))
 }
--- uno/main_test.go
+++ uno/main_test.go
@@ -7,8 +7,8 @@ import (
 func TestSonarSweep(t *testing.T) {
 	numbers := []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
 
-	t.Run("Assignment one example data", func(t *testing.T) {
-		got := NumOfIncreasesInSlice(numbers)
+	t.Run("Count increases between adjecant values in array", func(t *testing.T) {
+		got := CountIncreasesBetweenIndexes(numbers, 1)
 		want := 7
 
 		if got != want {
@@ -16,9 +16,8 @@ func TestSonarSweep(t *testing.T) {
 		}
 	})
 
-	t.Run("Assignment two example data", func(t *testing.T) {
-		nums := SumSliceByRange(numbers, 2)
-		got := NumOfIncreasesInSlice(nums)
+	t.Run("Count increases between adjecant+2 values in array", func(t *testing.T) {
+		got := CountIncreasesBetweenIndexes(numbers, 3)
 		want := 5
 
 		if got != want {
